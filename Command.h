732c44717740b20c304d4e030a879379b044ded0
Wasgo097 <patrpak906@student.polsl.pl>
2019-11-16 19:24:42 +0100

    Add command v0.1

---
+++ Command.h
@@ -0,0 +1,79 @@
+#pragma once
+#include <memory>
+#include <iostream>
+#include <string>
+#include <vector>
+typedef std::string string;
+class Mainwindow;
+class Command{
+	//std::shared_ptr<Mainwindow> window;
+	string _command;
+	void help(string temp) {
+		if (temp == "show") 
+			std::cout << "Mozesz wyswietlic:" << std::endl<<
+				"show process - lista procesow"<<std::endl<<
+				"show weather 'MIASTO' - aktualna pogoda z miasta 'MIASTO'"<<std::endl<<
+				"show memory - aktualne zuzycie pamieci ram"<<std::endl<<
+				"show cpu - aktualne zuzycie procesora"<<std::endl;
+		else if (temp == "kill") 
+			std::cout << "Zabija process o podanym id" << std::endl;
+		else if (temp == "clear") 
+			std::cout << "Czysci ekran konsoli" << std::endl;
+		else 
+			std::cout << "Nieznana komenda, uzyj help show, help kill, help clear" << std::endl;
+	}
+	void show(string temp,string city="") {
+		if (city == "") {
+			if (temp == "process") {
+
+			}
+			else if (temp == "cpur") {
+
+			}
+			else if (temp == "memory") {
+
+			}
+			else {
+				std::cout << "Nieznana komenda, uzyj help show";
+			}
+		}
+		else if (temp == "weather" && (!city.empty())) {
+
+		}
+		else {
+			std::cout << "Nieznana komenda, uzyj help show";
+		}
+	}
+	void kill(string id) {
+	}
+	void clear() {system("cls");}
+public:
+	Command(string cmd="") :_command(cmd) {}
+	string command() {return _command;}
+	void command(string x) {_command = x;}
+	void exec() {
+		std::vector<string> full_cmd;
+		string temp = "";
+		for (const auto&x:_command) {
+			if (x != ' ')
+				temp += x;
+			else {
+				full_cmd.push_back(temp);
+				temp.clear();
+			}
+		}
+		full_cmd.push_back(temp);
+		temp.clear();
+		if (full_cmd[0]=="show"){
+			if(full_cmd.size()==2) show(full_cmd[1]);
+			else show(full_cmd[1], full_cmd[2]);
+		}
+		else if (full_cmd[0] == "help") 
+			help(full_cmd[1]);
+		else if (full_cmd[0] == "kill") 
+			std::cout << "kill";
+		else std::cout << "Nieznana komenda";
+	}
+	~Command();
+};
+
--- Main.cpp
+++ Main.cpp
@@ -1,52 +1,55 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <thread>
-#include <chrono>
+#include <SFML/Graphics.hpp>
 #include <string>
+#include "Command.h"
 int main(){
-	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Yellow);
-	shape.setPosition(sf::Vector2f(150, 150));
-	while (window.isOpen()){
-		sf::Event event;
-		while (window.pollEvent(event)){
-			if (event.type == sf::Event::Closed||sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-				window.close();
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::RAlt)) {
-				/*std::string tekst;
-				getline(std::cin,tekst);
-				sf::Font font;
-				font.loadFromFile("Font.ttf");
-				sf::Text text;
-				text.setFont(font);
-				text.setString(tekst);*/
-				char c;
-				std::cin >> c;
-				switch (c)
-				{
-				case 'r':
-					shape.setFillColor(sf::Color::Red);
-					break;
-				case 'g':
-					shape.setFillColor(sf::Color::Green);
-					break;
-				case 'b':
-					shape.setFillColor(sf::Color::Blue);
-					break;
-				default:
-					shape.setFillColor(sf::Color::Yellow);
-				}
-				window.clear();
-				window.draw(shape);
-				//window.draw(text);
-				window.display();
-				std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-			}
-		}
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+	//sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	//sf::CircleShape shape(100.f);
+	//shape.setFillColor(sf::Color::Yellow);
+	//shape.setPosition(sf::Vector2f(150, 150));
+	//while (window.isOpen()){
+	//	sf::Event event;
+	//	while (window.pollEvent(event)){
+	//		if (event.type == sf::Event::Closed||sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+	//			window.close();
+	//		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::RAlt)) {
+	//			/*std::string tekst;
+	//			getline(std::cin,tekst);
+	//			sf::Font font;
+	//			font.loadFromFile("Font.ttf");
+	//			sf::Text text;
+	//			text.setFont(font);
+	//			text.setString(tekst);*/
+	//			char c;
+	//			std::cin >> c;
+	//			switch (c)
+	//			{
+	//			case 'r':
+	//				shape.setFillColor(sf::Color::Red);
+	//				break;
+	//			case 'g':
+	//				shape.setFillColor(sf::Color::Green);
+	//				break;
+	//			case 'b':
+	//				shape.setFillColor(sf::Color::Blue);
+	//				break;
+	//			default:
+	//				shape.setFillColor(sf::Color::Yellow);
+	//			}
+	//			window.clear();
+	//			window.draw(shape);
+	//			//window.draw(text);
+	//			window.display();
+	//			//std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+	//		}
+	//	}
+	//	window.clear();
+	//	window.draw(shape);
+	//	window.display();
+	//}
+	string x = "help show";
+	Command cmd(x);
+	cmd.exec();
+	std::cin.ignore(1);
 	return 0;
 }
